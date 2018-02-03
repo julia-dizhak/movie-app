fae769a3f570e78169ad3f8a5510eefa9e5ea866
Yulia <yulia.scherbina@gmail.com>
2018-02-03 20:50:27 +0100

    posts: posting data to the server

--- my-app/lessons/posts/components/NewPost/index.js
+++ my-app/lessons/posts/components/NewPost/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'react';
+import axios from 'axios';
 
 import './new-post.css';
 
@@ -16,6 +16,7 @@ class NewPost extends Component {
             body: this.state.content,
             author: this.state.author
         };
+
         axios.post('/posts', data)
             .then(response => {
                 console.log(response);
@@ -32,13 +33,14 @@ class NewPost extends Component {
                     type="text"
                     value={this.state.title}
                     onChange={(event) => this.setState({title: event.target.value})} />
+
                 <label>Content</label>
+                <textarea
+                    rows="4"
+                    value={this.state.content}
+                    onChange={(event) => this.setState({content: event.target.value})} />
 
-                <textarea rows="4"
-                          value={this.state.content}
-                          onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
-
                 <select
                     value={this.state.author}
                     onChange={(event) => this.setState({author: event.target.value})}>
