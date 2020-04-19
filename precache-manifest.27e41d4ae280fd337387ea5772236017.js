758a156ccd617567c27b6c6fc1d0d7d6de21d294
Yulia <yulia.scherbina@gmail.com>
2020-04-19 16:27:09 +0200

    Updates

---
+++ precache-manifest.27e41d4ae280fd337387ea5772236017.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba639be6d56fa0746f7ff32b630b2e5",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "3f84a5b7b419595b62d2",
+    "url": "/movie-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0dde9c6f5ba10a9e66ac",
+    "url": "/movie-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3f84a5b7b419595b62d2",
+    "url": "/movie-app/static/js/2.bf5a38f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movie-app/static/js/2.bf5a38f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dde9c6f5ba10a9e66ac",
+    "url": "/movie-app/static/js/main.9c013110.chunk.js"
+  },
+  {
+    "revision": "ef13b2d46e7e451c8c9d",
+    "url": "/movie-app/static/js/runtime-main.4744ba8c.js"
+  }
+]);
